1e5a5a6f54ac6b5765b9d68bbda484e48feddeba
Hoang To An <hoangtoan262003@gmail.com>
2023-11-17 16:35:53 +0700

    Add a feature for demonstration

--- OtherProjects/src/hust/soict/globalict/garbage/GarbageCreator.java
+++ OtherProjects/src/hust/soict/globalict/garbage/GarbageCreator.java
@@ -1,23 +1,39 @@
+// Hoàng Tố An - 20214980
 package hust.soict.globalict.garbage;
 
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.io.IOException;
 public class GarbageCreator {
 
-	public static void main(String[] args) 
-			throws IOException
+	public static void main(String[] args) throws IOException
 	{
 		String filename = "Example.txt";
 		byte[] inputBytes = {0};
 		long startTime, endTime;
-		Path fileName
-        = Path.of("C:\\Users\\Admin\\Desktop");
-		String str = Files.readString(fileName);
-		System.out.print(str);
-		String outputString = "";
 		
+		inputBytes = Files.readAllBytes(Paths.get(filename));
+		startTime = System.currentTimeMillis();
+		
+		// Dùng String
+		String outputString = "";
+		for (byte b : inputBytes) {
+			outputString += (char)b;
+		}
+		
+		// Dùng StringBuilder
+//		StringBuilder outputStringBuilder = new StringBuilder();
+//		for (byte b : inputBytes) {
+//			outputStringBuilder.append((char)b);
+//		}
+		
+		// Dùng StringBuffer
+//		StringBuffer outputStringBuffer = new StringBuffer();
+//		for (byte b : inputBytes) {
+//			outputStringBuffer.append((char)b);
+//		}
+		
+		endTime = System.currentTimeMillis();
+		System.out.println(endTime - startTime);
 	}
-
 }
